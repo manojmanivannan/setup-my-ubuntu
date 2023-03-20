715a64f3a2abcd772e96269f277b82c413be2817
Manoj Manivannan <manojm18@live.in>
2023-03-20 20:41:16 +0100

    update

--- installer/setup-docker.sh
+++ installer/setup-docker.sh
@@ -6,7 +6,8 @@ function setup_docker
 {
     print_green "Installing docker"
     curl -fsSL https://get.docker.com -o get-docker.sh
-    sudo sh get-docker.sh
-    sudo usermod -aG docker $USER
+    echo $SUDO_PASSWORD | sudo -S sh get-docker.sh
+    echo $SUDO_PASSWORD | sudo -s usermod -aG docker $USER
     rm get-docker.sh
+    apt_get_install docker-compose
 }
