98db067954d3bd4570af811d3f9c2f6249a6d884
Manoj Manivannan <manojm18@live.in>
2023-03-20 20:49:34 +0100

    update

---
+++ installer/setup-py.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function setup_py_env
+{
+  # PYTHON RELATED SETUP
+  print_green "Setting up PyENV"
+  apt_get_install build-essential libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python-tk python3-tk tk-dev
+  if [ -d "$HOME/.pyenv" ]; then
+    rm -rf "$HOME/.pyenv"
+  fi
+  curl https://pyenv.run | bash
+  mkdir -p $HOME/.scripts
+  cd $HOME/setup-my-ubuntu
+  cp etc/scripts/py_script.py $HOME/.scripts/py_script.py
+}
