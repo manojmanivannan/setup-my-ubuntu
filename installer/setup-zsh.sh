bd9ca3244834cf0767bf12c9fb6bbf2d774a4b67
Manoj Manivannan <manojm18@live.in>
2023-03-20 20:57:20 +0100

    update

---
+++ installer/setup-zsh.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function setup_via_zsh4humans
+{
+  print_green "Setup ZSH via zsh4humans";
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)";  # https://github.com/romkatv/zsh4humans 
+}
+
+function setup_via_oh_my_zsh
+{
+  print_green "Installing ZSH";
+  apt_get_install zsh
+  echo $SUDO_PASSWORD | sudo -S chsh $USER -s $(which zsh)
+  
+  print_green "Installing oh-my-zsh"
+
+  # If ~/.oh-y-zsh already exists, make a backup
+  if [ -d "$HOME/.oh-my-zsh" ]; then
+    echo "Backing up oh-my-zsh folder"
+    mv "$HOME/.oh-my-zsh" "$HOME/.oh-my-zsh-backup-$(date +%H_%M_%d_%h_%y)"
+  fi
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended ;
+  print_green "oh-my-zsh install complete"
+
+  # If ~/.zshrc already exists, make a backup
+  if [ -f "$HOME/.zshrc" ]; then
+    echo "Backing up $HOME/.zshrc"
+    mv "$HOME/.zshrc" "$HOME/.zshrc_backup_$(date +%H_%M_%d_%h_%y)"
+  fi
+
+  echo "Loading zshrc configurations to $HOME/.zshrc"
+  cp etc/.zshrc "$HOME/.zshrc"
+  cp etc/amuse.zsh-theme "$HOME/.oh-my-zsh/themes/amuse.zsh-theme"
+
+  echo "Loading GIT configuration to $HOME/.gitconfig"
+  cp etc/.gitconfig "$HOME/.gitconfig"
+
+  print_green "Setting up zsh plugins"
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
+  git clone https://github.com/DarrinTisdale/zsh-aliases-exa.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-aliases-exa
+  # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+  print_green "Setting up Fonts"
+  cd $HOME
+  git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git
+  cd nerd-fonts
+  git sparse-checkout add patched-fonts/JetBrainsMono && ./install.sh JetBrainsMono
+  cd $HOME/setup-my-ubuntu
+
+}
--- setup-system.sh
+++ setup-system.sh
@@ -14,6 +14,8 @@ set -e # Fail on any error
 source installer/setup-docker.sh
 source installer/setup-vscode.sh
 source installer/setup-py.sh
+source installer/setup-zsh.sh
+source installer/setup-java.sh
 source installer/print-helper.sh
 source installer/apt-helper.sh
 
@@ -66,101 +68,11 @@ function setup_ssh_key
 }
 
 
-function setup_java
-{
-  print_green "Setting up JAVA"
-  apt_get_update
-  apt_get_install default-jre default-jdk
-
-  TARGET_PROFILE="$HOME/.zshrc"
-
-  # JAVA HOME
-  if [[ "$(grep -q 'export JAVA_HOME' $TARGET_PROFILE)" -eq 1 ]]
-  then 
-    echo 'export JAVA_HOME=/usr/lib/jvm/default-java' >> "$TARGET_PROFILE"
-    echo 'export PATH=$JAVA_HOME/bin:$PATH' >> "$TARGET_PROFILE"
-  fi
-
-}
-
-function setup_spark
-{
-  local SPARK_VERSION="3.3.2"
-  local HADOOP_VERSION="3"
-  print_green "Setting up Spark $SPARK_VERSION with hadoop $HADOOP_VERSION"
-  setup_java
-  apt_get_install scala
-  if [[ ! -f "spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz" ]]
-  then
-    wget "https://dlcdn.apache.org/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz"
-  fi
-  tar xf spark-*
-  echo $SUDO_PASSWORD | sudo -S mv spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION /opt/spark
-  rm -rf spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
-  
-  TARGET_PROFILE="$HOME/.zshrc"
-
-  if [[ "$(grep -q 'export SPARK_HOME' $TARGET_PROFILE)" -eq 1 ]]
-  then 
-    echo 'export SPARK_HOME=/opt/spark' >> "$TARGET_PROFILE"
-    echo 'export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin' >> "$TARGET_PROFILE"
-    echo 'export PYSPARK_PYTHON=/usr/bin/python3' >> "$TARGET_PROFILE"
-  fi
-
-}
-
-function setup_via_zsh4humans
-{
-  print_green "Setup ZSH via zsh4humans";
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)";  # https://github.com/romkatv/zsh4humans 
-}
-
-function setup_via_oh_my_zsh
-{
-  print_green "Installing ZSH";
-  apt_get_install zsh
-  echo $SUDO_PASSWORD | sudo -S chsh $USER -s $(which zsh)
-  
-  print_green "Installing oh-my-zsh"
-
-  # If ~/.oh-y-zsh already exists, make a backup
-  if [ -d "$HOME/.oh-my-zsh" ]; then
-    echo "Backing up oh-my-zsh folder"
-    mv "$HOME/.oh-my-zsh" "$HOME/.oh-my-zsh-backup-$(date +%H_%M_%d_%h_%y)"
-  fi
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended ;
-  print_green "oh-my-zsh install complete"
-
-  # If ~/.zshrc already exists, make a backup
-  if [ -f "$HOME/.zshrc" ]; then
-    echo "Backing up $HOME/.zshrc"
-    mv "$HOME/.zshrc" "$HOME/.zshrc_backup_$(date +%H_%M_%d_%h_%y)"
-  fi
-
-  echo "Loading zshrc configurations to $HOME/.zshrc"
-  cp etc/.zshrc "$HOME/.zshrc"
-  cp etc/amuse.zsh-theme "$HOME/.oh-my-zsh/themes/amuse.zsh-theme"
-
-  echo "Loading GIT configuration to $HOME/.gitconfig"
-  cp etc/.gitconfig "$HOME/.gitconfig"
-
-  print_green "Setting up zsh plugins"
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  git clone https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
-  git clone https://github.com/DarrinTisdale/zsh-aliases-exa.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-aliases-exa
-  # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-  print_green "Setting up Fonts"
-  cd $HOME
-  git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git
-  cd nerd-fonts
-  git sparse-checkout add patched-fonts/JetBrainsMono && ./install.sh JetBrainsMono
-  cd $HOME/setup-my-ubuntu
 
 
 
-}
+
+
 
 if [ "$#" -lt 1 ]; then
   print_help
