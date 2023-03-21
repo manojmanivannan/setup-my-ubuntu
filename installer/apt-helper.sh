b37b45b8137c459449e6286009df93c78d991578
Manoj Manivannan <manojm18@live.in>
2023-03-21 10:07:36 +0100

    update

--- installer/apt-helper.sh
+++ installer/apt-helper.sh
@@ -12,7 +12,7 @@ function apt_get_install
   print_green "Installing $PACKAGE_NAMES"
   for PKG in $PACKAGE_NAMES; 
   do 
-    echo -e "\e[32m Package: '$PKG' \e[39m"  && echo $SUDO_PASSWORD | sudo -S apt-get install --ignore-missing -y $PKG  || echo -e "\e[31m ======> Unable to install '$PKG' package \e[39m"
+    text_yellow "'$PKG'"  && echo $SUDO_PASSWORD | sudo -S apt-get install --ignore-missing -y $PKG  || echo -e "\e[31m ======> Unable to install '$PKG' package \e[39m"
   done
 
 
