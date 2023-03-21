eb6dd770c7a065dff4d5cc02cd5c780b1b3f19dd
Manoj Manivannan <manojm18@live.in>
2023-03-21 10:15:28 +0100

    update

---
+++ installer/setup-ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+function setup_ssh_key
+{
+  echo -e "\nEnter your email ID for ssh key setup (Uses empty passphrase): "
+  read EMAIL_ID
+  ssh-keygen -t rsa -b 2048 -C "$EMAIL_ID" -f $HOME/.ssh/id_rsa
+}
