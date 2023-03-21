ce12b965341aaf0b9b584c107ae7d576732c0235
Manoj Manivannan <manojm18@live.in>
2023-03-21 10:37:42 +0100

    update

--- installer/apt-helper.sh
+++ installer/apt-helper.sh
@@ -6,7 +6,7 @@ function exit_on_failure
   local RET_CODE="$1"
   if [[ $RET_CODE -ne 0 ]]
   then
-    print_red "Script failed"
+    print_red "SCRIPT FAILED"
   fi
   exit 1
 }
@@ -15,6 +15,7 @@ function apt_get_update
 {
   print_green "Updating APT repos"
   echo $SUDO_PASSWORD | sudo -S apt-get update -y
+  exit_on_failure $?
 }
 
 function apt_get_install
@@ -23,7 +24,7 @@ function apt_get_install
   print_green "Installing $PACKAGE_NAMES"
   for PKG in $PACKAGE_NAMES; 
   do 
-    text_yellow "Package: '$PKG'"  && echo $SUDO_PASSWORD | sudo -S apt-get install -qq --ignore-missing -y $PKG  || echo -e "\e[31m ======> Unable to install '$PKG' package \e[39m"
+    text_yellow "Package: '$PKG'"  && echo $SUDO_PASSWORD | sudo -S apt-get install -qq --ignore-missing -y $PKG  || text_red "======> Unable to install '$PKG' package" && exit 1 ; exit_on_failure $?
   done
 
 
