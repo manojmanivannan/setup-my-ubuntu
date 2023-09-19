1f565047d2dd2288a3e6284ab76b651d208aeabb
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2023-09-19 14:27:17 +0000

    Add setup of python-pip

--- installer/setup-py.sh
+++ installer/setup-py.sh
@@ -3,6 +3,12 @@
 function setup_py_env
 {
   # PYTHON RELATED SETUP
+  print_green "Setting up Python/PIP"
+  PIP_INSTALL_FILE=/tmp/get-pip.py
+  PYTHON_EXE=$(which python3)
+  curl -o $PIP_INSTALL_FILE https://bootstrap.pypa.io/get-pip.py
+  $PYTHON_EXE $PIP_INSTALL_FILE
+  
   print_green "Setting up PyENV"
   apt_get_install build-essential libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python-tk python3-tk tk-dev
   if [ -d "$HOME/.pyenv" ]; then
