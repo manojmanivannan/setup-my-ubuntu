9d5d717be71ebe8a141ccfccc2b7c841e33183ac
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2023-09-21 14:52:58 +0100

    Update setup-py.sh

--- installer/setup-py.sh
+++ installer/setup-py.sh
@@ -11,6 +11,5 @@ function setup_py_env
   curl https://pyenv.run | bash
   exit_on_failure $? "Failed to setup PyENV"
   mkdir -p $HOME/.scripts
-  cd $HOME/setup-my-ubuntu
   cp etc/scripts/py_script.py $HOME/.scripts/py_script.py
 }
