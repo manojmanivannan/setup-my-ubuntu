7833048af43c6e364c41652ee0271b157855346f
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2023-09-28 15:37:14 +0100

    dont create docker group again

--- installer/setup-docker.sh
+++ installer/setup-docker.sh
@@ -5,7 +5,6 @@ function setup_docker
     print_green "Installing docker"
     curl -fsSL https://get.docker.com -o get-docker.sh
     echo $SUDO_PASSWORD | sudo -S sh get-docker.sh || exit_on_failure $? "Failed to install docker"
-    echo $SUDO_PASSWORD | sudo -S groupadd docker || exit_on_failure $? "Failed to create docker group"
     echo $SUDO_PASSWORD | sudo -S usermod -aG docker $USER || exit_on_failure $? "Failed to assign docker in sudo group"
     rm get-docker.sh
     apt_get_install docker-compose
