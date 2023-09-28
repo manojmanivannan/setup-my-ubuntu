304b2e35eb8d85b0aca9d661521421f779151f47
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2023-09-28 15:50:57 +0100

    install sublime from snap or apt

--- installer/setup-editor.sh
+++ installer/setup-editor.sh
@@ -21,9 +21,19 @@ function setup_vscode
 
 function setup_sublime_text
 {
-  apt_get_install dirmngr gnupg ca-certificates
-  curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-  sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
-  apt_get_update
-  apt_get_install sublime-text
+  print_green "Setting up Sublime text"
+
+  echo -en "Do you want to install Sublime text and Sublime merge from snap ? [Y/n]: "
+  read yn
+  case $yn in
+    [Yy]*)
+      echo $SUDO_PASSWORD | sudo -S snap install --classic sublime-text ;
+      echo $SUDO_PASSWORD | sudo -S snap install --classic sublime-merge ;;
+    [Nn]*) 
+      apt_get_install dirmngr gnupg ca-certificates ;
+      wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null ;
+      echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list ;
+      apt_get_update ;
+      apt_get_install sublime-text sublime-merge ;;
+  esac
 }
