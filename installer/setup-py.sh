a66be7b535a4c8ad1a541f80c8f8a021db846726
MANOJ MANIVANNAN <23746043+manojmanivannan@users.noreply.github.com>
2023-10-22 22:12:24 +0100

    Update setup-py.sh

--- installer/setup-py.sh
+++ installer/setup-py.sh
@@ -4,7 +4,7 @@ function setup_py_env
 {
   # PYTHON RELATED SETUP
   print_green "Setting up PyENV"
-  apt_get_install build-essential libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python-tk python3-tk tk-dev python3-pip python3-venv
+  apt_get_install build-essential libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python3-tk tk-dev python3-pip python3-venv
   pip_install inquirer
   if [ -d "$HOME/.pyenv" ]; then
     rm -rf "$HOME/.pyenv"
