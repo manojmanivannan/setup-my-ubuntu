6b005051946a189b581f783cfecf30927a966821
Manoj Manivannan <manojm18@live.in>
2025-09-26 21:47:21 +0100

    non interactive zsh installation

--- installer/setup-py.sh
+++ installer/setup-py.sh
@@ -18,7 +18,7 @@ function setup_py_env
 
   # PYTHON RELATED SETUP
   print_green "Setting up UV"
-  apt_get_install build-essential libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python3-tk tk-dev python3-pip python3-venv
+  apt_get_install_all build-essential libffi-dev libssl-dev libbz2-dev libreadline-dev libsqlite3-dev liblzma-dev python3-tk tk-dev python3-pip python3-venv
   pip_install inquirer
 
   curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -32,7 +32,7 @@ function setup_py_env
   fi
 
   echo 'eval "$(uv generate-shell-completion zsh)"' >> "$TARGET_PROFILE"
-  echo 'eval "$(uvx --generate-shell-completion bash)"' >> "$TARGET_PROFILE"
+  echo 'eval "$(uvx --generate-shell-completion zsh)"' >> "$TARGET_PROFILE"
 
 }
 
@@ -46,6 +46,16 @@ function uninstall_py_env
     print_yellow "UV is not installed, skipping uninstallation"
     return
   fi
+
+  #remove uv related lines from .zshrc or .bashrc
+  TARGET_PROFILE="$HOME/.zshrc"
+  if ! [[ -f $TARGET_PROFILE ]]
+  then
+    TARGET_PROFILE="$HOME/.bashrc"
+  fi
+  sed -i '/uv generate-shell-completion zsh/d' "$TARGET_PROFILE"
+  sed -i '/uvx --generate-shell-completion zsh/d' "$TARGET_PROFILE"
+
   uv cache clean
   rm -rf "$(uv python dir)"
   rm -rf "$(uv tool dir)"
