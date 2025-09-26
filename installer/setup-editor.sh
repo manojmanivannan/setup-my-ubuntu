3231bfb8c585d77fa4eeb88b2749dae866f3a5ce
Manoj Manivannan <manojm18@live.in>
2025-09-26 22:17:36 +0100

    improve vscode install

--- installer/setup-editor.sh
+++ installer/setup-editor.sh
@@ -23,10 +23,9 @@ function setup_vscode
   case $yn in
     [Yy]*) echo $SUDO_PASSWORD | sudo -S snap install --classic code ;;
     [Nn]*) 
-      wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg;
-      echo $SUDO_PASSWORD | sudo -S install -D -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg;
-      echo $SUDO_PASSWORD | sudo -S sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list';
-      rm -f /tmp/packages.microsoft.gpg;
+      wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
+      echo $SUDO_PASSWORD | sudo -S install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg;
+      rm -f microsoft.gpg
       apt_get_update;
       apt_get_install code ;;
   esac
